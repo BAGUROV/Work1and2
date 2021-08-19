b95a9cff9cd00821e4e5ba3c8abb224d84d230f5
BAGUROV <55102027+BAGUROV@users.noreply.github.com>
2021-08-19 11:32:26 +0300

    Update SQLCode.sql

--- Work2/SQLCode.sql
+++ Work2/SQLCode.sql
@@ -1,3 +1,3 @@
 SELECT Product.Name as "Название продукта", STRING_AGG(Category.Name, ',') as "Категории" FROM Product 
-INNER JOIN Category ON Product.Id_Сategory = Category.Id
+LEFT JOIN Category ON Product.Id_Сategory = Category.Id
 GROUP BY Product.Name;
